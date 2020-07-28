800ae48a7639bb42b75e967229b12a8db91aa525
Anney Park <anneypark@gmail.com>
2020-07-28 01:48:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     if src[row_index][element_index].class == String
-      sentence += src[row_index][element_index] + ''
+      sentence += src[row_index][element_index] + ""
     end
     element_index += 1
 end
