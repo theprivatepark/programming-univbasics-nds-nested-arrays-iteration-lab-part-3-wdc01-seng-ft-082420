de9e781252d3e5c5d8f251d60f58ac5f113df09b
Anney Park <anneypark@gmail.com>
2020-07-28 01:27:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,9 +1,9 @@
 def join_nested_strings(src)
-total = 0
+sentence = 0
 row_index = 0
-while row_index < guessing_game_grid.count do
+while row_index < src.count do
   element_index = 0
-  while element_index < guessing_game_grid[row_index].count do
+  while element_index < src[row_index].count do
     total += guessing_game_grid[row_index][element_index]
     element_index += 1
   end
